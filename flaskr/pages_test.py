dc648400898b47f83d0ec672391e4c0e46b81e38
vincent_quock <vincent_quock@techexchange.in>
2023-04-19 06:05:08 +0000

    bad idea

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -84,20 +84,24 @@ def test_about(client, mock_backend):
 
 
 def test_welcome(client):
+    """Explain: Test if a person can access welcome page"""
     resp = client.get("/welcome")
     assert resp.status_code == 200
     assert b"Welcome" in resp.data
 
 def test_get_login(client):
+    """Explain: Test if a person can access login page"""
     resp = client.get("/login")
     assert resp.status_code == 200
     assert b"<h1>Login</h1>" in resp.data
 
 def test_comments_upload(client,mock_backend):
+    """Explain: Test if a person can post a comment"""
     resp = client.post("/comments", data = {"comment": "helloworld", "hidden":"sandy"})
     mock_backend.upload_comment.assert_called_once()
 
 def test_comments_view(client,mock_backend):
+    """Explain: Test if a person can access comment page"""
     resp = client.get("/comments")
     assert resp.status_code ==200
     assert b"Post your comment here!" in resp.data
@@ -105,6 +109,7 @@ def test_comments_view(client,mock_backend):
 
 @patch("uuid.uuid4")
 def test_auth_login_success(mock_uuid, client, mock_backend):
+    """Explain: Test if login works"""
     mock_uuid.return_value = "1234"
     mock_backend.sign_in.return_value = (True, "Test Name")
     resp = client.post("/auth_login",
@@ -118,6 +123,7 @@ def test_auth_login_success(mock_uuid, client, mock_backend):
 
 
 def test_auth_login_fail(client, mock_backend):
+    """Explain: Test if login fails"""
     mock_backend.sign_in.return_value = (False, "Test Name")
     resp = client.post("/auth_login", data={"Username": "U", "Password": "P"})
     assert resp.status_code == 200
@@ -125,18 +131,21 @@ def test_auth_login_fail(client, mock_backend):
 
 
 def test_logout(client, mock_backend):
+    """Explain: Test if a person can log out"""
     resp = client.get("/logout", follow_redirects=True)
     assert resp.status_code == 200
     assert b"Music Theory Wiki" in resp.data
 
 
 def test_upload_page(client):
+    """Explain: Test if a person can access upload page """
     resp = client.get("/upload")
     assert resp.status_code == 200
     assert b"*Only accepts .jpg, .png" in resp.data
 
 
 def test_upload_success(client, mock_backend):
+    """Explain: Test if upload works"""
     file_ = FileStorage(filename="test_dir/test_file.md",
                         content_type="text/markdown")
     resp = client.post("/upload", data={"upload": file_}, follow_redirects=True)
@@ -147,6 +156,7 @@ def test_upload_success(client, mock_backend):
 
 
 def test_upload_fail(client):
+    """Explain: Test if upload fails"""
     file_ = FileStorage(filename="test_dir/test_file.bad")
     resp = client.post("/upload", data={"upload": file_}, follow_redirects=True)
 
@@ -155,23 +165,27 @@ def test_upload_fail(client):
 
 
 def test_get_signup(client):
+    """Explain: Test if a person can access signup page"""
     resp = client.get("/signup")
     assert resp.status_code == 200
     assert b"<h1>Sign Up</h1>" in resp.data
 
 # if file format is not (.jpg) (.jpeg) (.png) or (.md) assert error
 def test_get_about(client):
+    """Explain: Test if a person can access about page"""
     resp = client.get("/about")
     assert resp.status_code == 200  #This check that the connection to about is good
     assert b"Your Authors" in resp.data  #This check if the cilent can grabs the data within about
 
 def test_get_page_summary(client, mock_backend):
+    """Explain: Test if a person can get summary from get_wiki_page"""
     mock_backend.get_wiki_page.return_value=("Chord is a group of notes","a group of notes")
     resp = client.get("/pages/sub_pages")
     assert resp.status_code == 200  #This check that the connection to about is good
     assert b"a group of notes" in resp.data
 
 def test_get_page_summary_None(client, mock_backend):
+    """Explain: Test if a person can get summary from get_wiki_page if it is None"""
     test_main = "Chord is a group of notes"
     main_len=len(test_main)
     mock_backend.get_wiki_page.return_value=("Chord is a group of notes",None)
@@ -191,6 +205,7 @@ def test_get_page_summary_None(client, mock_backend):
 
 @patch("uuid.uuid4")
 def test_signup_success(mock_uuid, login_client, mock_backend):
+    """Explain: Test if the page when signup is successful displays"""
     mock_backend.sign_up.return_value = (True, "Test Name")
     mock_uuid.return_value = "1234"
     resp = login_client.post("/auth_signup",
@@ -204,6 +219,7 @@ def test_signup_success(mock_uuid, login_client, mock_backend):
     assert b"Welcome Test Name" in resp.data
 
 def test_get_welcome(client, mock_backend):
+    """ExplainL Test if a person can access welcome page"""
     resp = client.get("/welcome")
     assert resp.status_code == 200  #This check that the connection to welcome is good
     assert b"Welcome" in resp.data  #This check if the cilent can grabs the data within welcome
@@ -218,6 +234,7 @@ This would be my test for the update but I'm not sure how I could mock a logged 
 #     assert b"History" in resp.data  #This check if the cilent can grabs the data within about
 '''
 def test_signup_fail(client, mock_backend):
+    """Explain: Test if the page when signup doesn't work displays"""
     mock_backend.sign_up.return_value = (False, "Test Name")
     resp = client.post("/auth_signup",
                        data={
@@ -231,6 +248,7 @@ def test_signup_fail(client, mock_backend):
 
 
 def test_get_allimages(client, mock_backend):
+    """Example: Check if get_image from backend works"""
     mock_backend.get_image.return_value = ["pic0", "pic1"]
     resp = client.get("/images")
     assert resp.status_code == 200
@@ -239,6 +257,7 @@ def test_get_allimages(client, mock_backend):
 
 
 def test_invalid_method(client, mock_backend):
+    """Example: Test if a user cannot access a method"""
     mock_backend.get_image = lambda: abort(405)
     resp = client.get("/images", follow_redirects=True)
     assert resp.status_code == 405
@@ -246,6 +265,7 @@ def test_invalid_method(client, mock_backend):
 
 
 def test_page_sort_alpha(client, mock_backend):
+    """Example: Test if get_all_page_name sorts alphabetically """
     mock_backend.get_all_page_names.return_value = ['a_test', 'b_test', 'c_test']
     resp = client.get('/pages', query_string={'sort_by': 'Alphabetical'})
     
@@ -256,6 +276,7 @@ def test_page_sort_alpha(client, mock_backend):
 
 
 def test_page_sort_pop(client, mock_backend):
+    """Example: Test if sort_by_popularity sorts by popularity """
     mock_backend.page_sort_by_popularity.return_value = ['3_test', '2_test', '1_test']
     resp = client.get('/pages', query_string={'sort_by': 'Popularity'})
 
